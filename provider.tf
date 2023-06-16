b08ee726590364ec3236c5ebd1ba2ee31cca7263
Eli Lowinger <low613@gmail.com>
2023-06-16 13:55:07 +1000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.29.0"
+      version = ">= 4.29.0"
     }
 
     cloudflare = {
         source = "cloudflare/cloudflare"
-        version = "3.23.0"
+        version = ">= 3.23.0"
     }
   }
 }
